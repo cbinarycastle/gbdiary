571bee1bd53d9e1392a5a8f58116ac713d156834
cbinarycastle <cbinarycastle@gmail.com>
2022-06-27 23:09:13 +0900

    feat: Timber 로깅 설정 추가

---
+++ app/src/main/java/io/github/cbinarycastle/diary/DiaryApplication.kt
@@ -0,0 +1,12 @@
+package io.github.cbinarycastle.diary
+
+import android.app.Application
+import timber.log.Timber
+
+class DiaryApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+}
