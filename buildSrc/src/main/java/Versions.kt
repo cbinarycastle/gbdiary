eaced170049181243c0bf07ea74ba3eb9e22fa72
cbinarycastle <cbinarycastle@gmail.com>
2022-09-22 15:43:41 +0900

    build: v1.0 업데이트

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -1,9 +1,8 @@
 object Versions {
     private const val VERSION_MAJOR = 1
     private const val VERSION_MINOR = 0
-    private const val VERSION_PATCH = 9
-    const val VERSION_CODE = VERSION_MAJOR * 10000 + VERSION_MINOR * 100 + VERSION_PATCH
-    const val VERSION_NAME = "$VERSION_MAJOR.$VERSION_MINOR.$VERSION_PATCH"
+    const val VERSION_CODE = VERSION_MAJOR * 100 + VERSION_MINOR
+    const val VERSION_NAME = "$VERSION_MAJOR.$VERSION_MINOR"
 
     const val COMPILE_SDK = 32
     const val TARGET_SDK = 32
